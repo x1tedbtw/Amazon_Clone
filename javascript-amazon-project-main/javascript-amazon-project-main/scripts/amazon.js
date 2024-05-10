4268cd1a7d5e2f6f92a18fe0fb6a943a27f5b5e3
x1tedbtw <nefedov.vitali@gmail.com>
2024-05-10 17:19:58 +0200

    Added a cart.js file

--- javascript-amazon-project-main/javascript-amazon-project-main/scripts/amazon.js
+++ javascript-amazon-project-main/javascript-amazon-project-main/scripts/amazon.js
@@ -46,14 +46,24 @@ products.forEach((product) => {
         Added
         </div>
 
-        <button class="add-to-cart-button button-primary">
+        <button class="add-to-cart-button button-primary js-add-to-cart" data-product-name="${product.name}">
         Add to Cart
         </button>
     </div>`
 });
 
-console.log(productsHTML);
 
 document.querySelector('.js-products-grid')
     .innerHTML = productsHTML;
 
+document.querySelectorAll('.js-add-to-cart')
+    .forEach((button) => {
+        button.addEventListener('click', () => {
+            const productName = button.dataset.productName;
+            cart.push({
+                productName: productName,
+                quantity: 1
+            });
+            console.log(cart);
+        });
+    });
