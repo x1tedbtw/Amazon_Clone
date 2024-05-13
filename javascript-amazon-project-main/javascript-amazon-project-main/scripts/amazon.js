7fe2bfa1104dd9c3b004e0895b6e595f1f0510f7
x1tedbtw <nefedov.vitali@gmail.com>
2024-05-13 12:43:25 +0200

    Made the cart quantity interactive

--- javascript-amazon-project-main/javascript-amazon-project-main/scripts/amazon.js
+++ javascript-amazon-project-main/javascript-amazon-project-main/scripts/amazon.js
@@ -78,6 +78,14 @@ document.querySelectorAll('.js-add-to-cart')
                 });
             }
 
-            console.log(cart);
+            let cartQuantity = 0;
+
+            cart.forEach((item) => {
+                cartQuantity += item.quantity;
+            });
+
+            document.querySelector('.js-cart-quantity')
+                .innerHTML = cartQuantity;
+
         });
     });
